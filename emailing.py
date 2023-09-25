cb9982141938ba8cf160f6d24ccccb36f5048bd9
kostas-python <diamantopoulos.kostas@hotmail.com>
2023-09-25 23:11:15 +0100

    Day 36: Webcam app completed

--- emailing.py
+++ emailing.py
@@ -1,2 +1,31 @@
-def send_email():
-    print("Email was sent")
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
+PASSWORD = "vlreoccbsrgcsire"
+SENDER = "kostas.com2@gmail.com"
+RECEIVER = "diamantopoulos.kostas@hotmail.com"
+
+
+def send_email(image_path):
+    print("send email function started")
+    email_message = EmailMessage()
+    email_message["Subject"] = "New customer showed up!"
+    email_message.set_content("Hey, we just saw a new customer")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, PASSWORD, email_message.as_string())
+    gmail.quit()
+    print("send_email function ended")
+
+
+if __name__ == "__main__":
+    send_email(image_path="images/20.png")
+
